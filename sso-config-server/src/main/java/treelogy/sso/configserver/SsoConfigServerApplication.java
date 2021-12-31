c9d8b9e295be3f38a5bddf20917709f6dbc55b36
CezarFelipe <cezarfelipedasilva@gmail.com>
2021-12-30 21:02:00 -0300

    create microservice config-server

---
+++ sso-config-server/src/main/java/treelogy/sso/configserver/SsoConfigServerApplication.java
@@ -0,0 +1,13 @@
+package treelogy.sso.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsoConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsoConfigServerApplication.class, args);
+	}
+
+}
---
+++ sso-config-server/src/test/java/treelogy/sso/configserver/SsoConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package treelogy.sso.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsoConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
