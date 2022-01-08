7378eb69464c76e2f9bdd2cb32dd330ec4dc003e
CezarFelipe <cezarfelipedasilva@gmail.com>
2022-01-08 04:00:11 -0300

    Create Server Registry

---
+++ sso-service-registry/src/main/java/treelogy/sso/serviceregistry/SsoServiceRegistryApplication.java
@@ -0,0 +1,13 @@
+package treelogy.sso.serviceregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsoServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsoServiceRegistryApplication.class, args);
+	}
+
+}
---
+++ sso-service-registry/src/test/java/treelogy/sso/serviceregistry/SsoServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package treelogy.sso.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsoServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
