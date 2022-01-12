a4df9a4a24381b6c0d74d1c02f977a39dadc7437
CezarFelipe <cezarfelipedasilva@gmail.com>
2022-01-12 10:57:18 -0300

    incluide wso2 server

---
+++ sso-api-gateway/src/main/java/treelogy/sso/apigateway/SsoApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package treelogy.sso.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsoApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsoApiGatewayApplication.class, args);
+	}
+
+}
