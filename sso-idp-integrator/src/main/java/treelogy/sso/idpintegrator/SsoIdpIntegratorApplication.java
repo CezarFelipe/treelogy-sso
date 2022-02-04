e13246447eee45e22882f0badfa8eec1051881d1
CezarFelipe <cezarfelipedasilva@gmail.com>
2022-02-03 22:08:04 -0300

    create microservice idp-integrator

---
+++ sso-idp-integrator/src/main/java/treelogy/sso/idpintegrator/SsoIdpIntegratorApplication.java
@@ -0,0 +1,13 @@
+package treelogy.sso.idpintegrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsoIdpIntegratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsoIdpIntegratorApplication.class, args);
+	}
+
+}
---
+++ sso-idp-integrator/src/test/java/treelogy/sso/idpintegrator/SsoIdpIntegratorApplicationTests.java
@@ -0,0 +1,13 @@
+package treelogy.sso.idpintegrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsoIdpIntegratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
