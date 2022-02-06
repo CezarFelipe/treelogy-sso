c223a2a6827e11d0a4e027cc7acb4f3fca08a6ed
CezarFelipe <cezarfelipedasilva@gmail.com>
2022-02-06 11:11:13 -0300

    configure api wso2

--- sso-api-wso2/src/main/java/treelogy/sso/apiwso2/SsoApiWso2Application.java
+++ sso-api-wso2/src/main/java/treelogy/sso/apiwso2/SsoApiWso2Application.java
@@ -2,7 +2,9 @@ package treelogy.sso.apiwso2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class SsoApiWso2Application {
 
